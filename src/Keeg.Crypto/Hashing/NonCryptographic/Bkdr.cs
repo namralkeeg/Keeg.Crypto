1e1a85163837ffc3eaea96a928e8796cc98cc158
Larry Lopez <namralkeeg@gmail.com>
2018-05-31 12:46:35 -0700

    Added Bkdr hashing algorithm.

---
+++ src/Keeg.Crypto/Hashing/NonCryptographic/Bkdr.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Keeg.Crypto.Hashing.NonCryptographic
+{
+    /// <summary>
+    /// This hash function comes from Brian Kernighan and Dennis Ritchie's book "The C Programming Language".
+    /// </summary>
+    public sealed class Bkdr : HashAlgorithm
+    {
+        private uint seed;
+        private uint hash;
+
+        public Bkdr()
+        {
+            /// 31 131 1313 13131 131313 etc..
+            seed = 131u;
+            Initialize();
+        }
+
+        new static public Bkdr Create()
+        {
+            return Create(typeof(Bkdr).Name);
+        }
+
+        new static public Bkdr Create(string hashName)
+        {
+            return (Bkdr)HashAlgorithmFactory.Create(hashName);
+        }
+
+        public override void Initialize()
+        {
+            hash = seed;
+        }
+
+        protected override void HashCore(byte[] array, int ibStart, int cbSize)
+        {
+            for (var i = ibStart; i < ibStart + cbSize; i++)
+            {
+                hash = (hash * seed) + array[i];
+            }
+        }
+
+        protected override byte[] HashFinal()
+        {
+            return BitConverter.GetBytes(hash);
+        }
+
+        public uint Seed { get => seed; set => seed = value; }
+    }
+}
