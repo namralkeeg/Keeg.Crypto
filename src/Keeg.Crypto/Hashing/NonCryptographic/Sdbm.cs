41c3670cab44718c70d7d081501450dd60d2a8ee
Larry Lopez <namralkeeg@gmail.com>
2018-05-31 18:56:57 -0700

    Added the Sdbm hashing algorithm.

---
+++ src/Keeg.Crypto/Hashing/NonCryptographic/Sdbm.cs
@@ -0,0 +1,73 @@
+﻿#region Copyright
+/*
+ * Copyright (C) 2018 Larry Lopez
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+#endregion
+using System;
+using System.Security.Cryptography;
+
+namespace Keeg.Crypto.Hashing.NonCryptographic
+{
+    /// <summary>
+    /// The algorithm of choice which is used in the open source SDBM project.
+    /// </summary>
+    public sealed class Sdbm : HashAlgorithm
+    {
+        private uint hash;
+
+        public Sdbm()
+        {
+            HashSizeValue = 32;
+            Initialize();
+        }
+
+        public static new Sdbm Create()
+        {
+            return Create(typeof(Sdbm).Name);
+        }
+
+        public static new Sdbm Create(string hashName)
+        {
+            return (Sdbm)HashAlgorithmFactory.Create(hashName);
+        }
+
+        public override void Initialize()
+        {
+            hash = 0;
+        }
+
+        protected override void HashCore(byte[] array, int ibStart, int cbSize)
+        {
+            for (var i = ibStart; i < ibStart + cbSize; i++)
+            {
+                unchecked
+                {
+                    hash = array[i] + (hash << 6) + (hash << 16) - hash;
+                }
+            }
+        }
+
+        protected override byte[] HashFinal()
+        {
+            return BitConverter.GetBytes(hash);
+        }
+    }
+}
