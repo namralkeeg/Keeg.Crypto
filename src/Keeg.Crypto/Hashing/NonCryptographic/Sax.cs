e89369de2b56ac0d33ae6b334741dff23f7b1e09
Larry Lopez <namralkeeg@gmail.com>
2018-05-31 18:31:07 -0700

    Added Rs, and Sax hashing algorithms.

---
+++ src/Keeg.Crypto/Hashing/NonCryptographic/Sax.cs
@@ -0,0 +1,76 @@
+﻿#region Copyright
+/*
+ * Copyright (C) 2018 Larry Lopez
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+#endregion
+using System;
+using System.Security.Cryptography;
+
+namespace Keeg.Crypto.Hashing.NonCryptographic
+{
+    /// <summary>
+    /// Shift-Add-XOR hash
+    /// </summary>
+    public sealed class Sax : HashAlgorithm
+    {
+        private uint hash;
+
+        public Sax()
+        {
+            HashSizeValue = 32;
+            Seed = 0;
+            Initialize();
+        }
+
+        public static new Sax Create()
+        {
+            return Create(typeof(Sax).Name);
+        }
+
+        public static new Sax Create(string hashName)
+        {
+            return (Sax)HashAlgorithmFactory.Create(hashName);
+        }
+
+        public uint Seed { get; set; }
+
+        public override void Initialize()
+        {
+            hash = Seed;
+        }
+
+        protected override void HashCore(byte[] array, int ibStart, int cbSize)
+        {
+            for (var i = ibStart; i < ibStart + cbSize; i++)
+            {
+                unchecked
+                {
+                    hash ^= (hash << 5) + (hash >> 2) + array[i];
+                }
+            }
+        }
+
+        protected override byte[] HashFinal()
+        {
+            return BitConverter.GetBytes(hash);
+        }
+    }
+}
