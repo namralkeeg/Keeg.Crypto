87ddfd78cdcd990c9099cc968dc7609c598a0041
Larry Lopez <namralkeeg@gmail.com>
2018-06-02 13:46:51 -0700

    Added BitSize enum to support the variable bit size Fletcher checksum algorithm.

---
+++ src/Keeg.Crypto/Common/BitSize.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Keeg.Crypto.Common
+{
+    public enum BitSize : int
+    {
+        Bits16 = 16,
+        Bits32 = 32,
+        Bits64 = 64,
+        Bits128 = 128,
+        Bits256 = 256,
+        Bits512 = 512,
+        Bits1024 = 1024,
+        Bits2048 = 2048,
+        Bits4096 = 4096,
+    }
+}
